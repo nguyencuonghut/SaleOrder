83180945c942a1111eb5af811f38ee087a2a256a
Tony Ng <nguyencuonghut55@gmal.com>
2023-10-20 16:27:58 +0700

    Fix: the bug sometimes cannot click cart drop-down

--- resources/views/livewire/count-cart.blade.php
+++ resources/views/livewire/count-cart.blade.php
@@ -3,18 +3,17 @@
       <i class="fas fa-shopping-cart"></i>
       <span class="badge badge-success navbar-badge">{{ number_format(Cart::getContent()->count(), 0, '.', ',') }}</span>
     </a>
-    @if(Cart::getContent()->count())
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <span class="dropdown-item dropdown-header">{{ number_format(Cart::getTotalQuantity(), 0, '.', ',') }} KG cho {{Cart::getContent()->count()}}  mã hàng</span>
-        @foreach (Cart::getContent() as $item)
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-                {{$item->name}}
-                <span class="float-right text-muted text-sm">{{ number_format($item->quantity, 0, '.', ',') }} KG</span>
-            </a>
-        @endforeach
+
+    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+    <span class="dropdown-item dropdown-header">{{ number_format(Cart::getTotalQuantity(), 0, '.', ',') }} KG cho {{Cart::getContent()->count()}}  mã hàng</span>
+    @foreach (Cart::getContent() as $item)
         <div class="dropdown-divider"></div>
-        <a href="{{route('cart.detail')}}" wire:navigate class="dropdown-item dropdown-footer">Xem giỏ hàng</a>
-        </div>
-    @endif
+        <a href="#" class="dropdown-item">
+            {{$item->name}}
+            <span class="float-right text-muted text-sm">{{ number_format($item->quantity, 0, '.', ',') }} KG</span>
+        </a>
+    @endforeach
+    <div class="dropdown-divider"></div>
+    <a href="{{route('cart.detail')}}" wire:navigate class="dropdown-item dropdown-footer">Xem giỏ hàng</a>
+    </div>
   </li>
