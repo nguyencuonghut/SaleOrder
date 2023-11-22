5719ed130b5ef0d3d4283a470e1099a9e41c989a
Tony Ng <nguyencuonghut55@gmal.com>
2023-11-22 11:34:58 +0700

    Fix:  improve the notification email when the order is approved

--- app/Notifications/OrderApproved.php
+++ app/Notifications/OrderApproved.php
@@ -3,6 +3,7 @@
 namespace App\Notifications;
 
 use App\Models\Order;
+use App\Models\OrdersProducts;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -38,10 +39,15 @@ class OrderApproved extends Notification implements ShouldQueue
     public function toMail(object $notifiable): MailMessage
     {
         $order = Order::findOrFail($this->order_id);
+        $ordersproducts = OrdersProducts::where('order_id', $this->order_id)->where('is_deleted', false)->get();
         $url = url('/orders/'.$order->id);
         return (new MailMessage)
-                    ->subject('Kết quả duyệt đơn đặt hàng')
-                    ->markdown('email.order.approved', ['order' => $order, 'url' => $url, 'level' => $this->level]);
+                    ->subject('Kết quả duyệt đơn đặt hàng ' . $ordersproducts->count() . ' sản phẩm, tổng trọng lượng ' . number_format($order->total_weight, 0, '.', ',') . ' KG')
+                    ->markdown('email.order.approved',
+                                ['order' => $order,
+                                 'order_products_cnt' => $ordersproducts->count(),
+                                 'url' => $url, 'level' => $this->level
+                                ]);
     }
 
     /**
--- resources/views/email/order/approved.blade.php
+++ resources/views/email/order/approved.blade.php
@@ -2,12 +2,15 @@
 Xin chào,
 
 Đơn đặt hàng của bạn đã được duyệt.<br>
+Kỳ: {{$order->schedule->title}}.<br>
+Số sản phẩm: {{$order_products_cnt}}.<br>
+Tổng trọng lượng: {{number_format($order->total_weight, 0, '.', ',')}} KG.<br>
 @if ('level_1' == $level)
     @if('Đồng ý' == $order->level1_manager_approved_result)
     - Kết quả: <b style="color:green;">{{$order->level1_manager_approved_result}}</b>.<br>
     - Trạng thái: <b style="color:blue;">{{$order->status}}</b>.
     @else
-        - Kết quả: <b style="color:red;">{{$order->level1_manager_approved_result}}</b>.
+    - Kết quả: <b style="color:red;">{{$order->level1_manager_approved_result}}</b>.
     @endif
 @elseif('level_2' == $level)
     @if('Đồng ý' == $order->level2_manager_approved_result)
