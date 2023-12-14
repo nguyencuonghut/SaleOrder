4cdef598ea61b43247e8c38a4fe78ab9b8337cf2
Tony Ng <nguyencuonghut55@gmal.com>
2023-12-14 09:14:05 +0700

    Auto stash before revert of "Fix: correct the mistake on login function"

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PhpOffice\PhpSpreadsheet\Calculation\Logical\Boolean;
 
 class User extends Authenticatable
 {
@@ -53,5 +54,10 @@ class User extends Authenticatable
     public function orders()
     {
         return $this->hasMany(Order::class);
+    }
+
+    public function isDisabled()
+    {
+        return 'Vô hiệu' == $this->status ? true : false;
     }
 }
